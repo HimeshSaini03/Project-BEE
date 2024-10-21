81d245f59d77dcde97be4c8d83a0fb6649524e10
Himesh <himeshsainichd@gmail.com>
2024-10-21 11:41:09 +0530

    ok

--- Server.js
+++ Server.js
@@ -19,9 +19,15 @@
 const express=require("express");
 const connectDb=require("./config/dbConnection");
 const errorHandler=require("./middleware/errorHandler");
-const cors=require("cors");
+const cors("cors");
 connectDb();
 const app=express ();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
+
+app.use(errorHandler);
+
+app.listen(port ,() => {
+    console.log(`Server running on port https://localhost:${port}`);
+});
