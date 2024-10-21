b858e4df15069b9ed79fe1a4c6338907fae3b993
Himesh <himeshsainichd@gmail.com>
2024-10-21 11:32:43 +0530

    server

--- Server.js
+++ Server.js
@@ -1,9 +1,27 @@
-const express = require('express')
-const connectDb = require("")
+// const express = require('express');
+// const app = express();
 
-const app = express()
+// // Define the port
+// const PORT = 3000;
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+// // Create a basic route
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
+
+// // Server listens on the defined port
+// app.listen(PORT, () => {
+//   console.log(Server running at http://localhost:${PORT}/);
+// });
+
+
+// FRAMEWORK CONFIGURATION
+const express=require("express");
+const connectDb=require("./config/dbConnection");
+const errorHandler=require("./middleware/errorHandler");
+const cors=require("cors");
+connectDb();
+const app=express ();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
