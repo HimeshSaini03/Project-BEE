d15b12c19c786b5af5d31f905836ffcd7124c0c4
Himesh <himesh@gmail.com>
2024-11-05 10:56:04 +0530

    Day (4&5)

--- Project-BEE-main/Server/Server.js
+++ Project-BEE-main/Server/Server.js
@@ -1,83 +1,110 @@
-  // const express = require('express');
-  // const app = express();
+// const express = require('express');
+// const app = express();
 
-  // // Define the port
-  // const PORT = 3000;
+// // Define the port
+// const PORT = 3000;
 
-  // // Create a basic route
-  // app.get('/', (req, res) => {
-  //   res.send('Hello, Express!');
-  // });
-
-
+// // Create a basic route
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
 
 
-  // FRAMEWORK CONFIGURATION
-
-  const express = require("express");
-  const connectDb = require("./config/dbConnection"); // Ensure this path is correct
-  //const errorHandler = require("./middleware/errorHandler"); // Ensure this path is correct
-  const cors = require("cors");
-  const hbs=require("hbs")
-  // Connect to the database
-
-  // Create an Express application
-  // Middleware
-  // app.use(express.json());
-  // app.use(cors());
-
-  // Basic route
-  // app.get("/", (req, res) => {
-  //     res.send("Hello World");
-  // });
-
-  // Error handling middleware
-  // app.use(errorHandler);
-
-  // Start the server
-  // app.listen(port, () => {
-  //     console.log(Server is running on port ${port});
-  // });
-
-  // env file configz
-  const dotenv=require("dotenv");
-  dotenv.config();
-  connectDb();
-  const app = express();
-  app.set('view engine','hbs');
-
-  hbs.registerPartials(__dirname + '/views/partials'); // Path to your partials
 
 
-  app.get("/home",(req,res)=>{
+// FRAMEWORK CONFIGURATION
 
-      res.render("home",{
-          username:"Himesh",
-          
-      })
+const express = require("express");
+const connectDb = require("./config/dbConnection"); // Ensure this path is correct
+const errorHandler = require("./middleware/errorHandler"); // Ensure this path is correct
+const cors = require("cors");
+const hbs=require("hbs")
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+// Connect to the database
+
+// Create an Express application
+// Middleware
+
+
+// Basic route
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// });
+
+// Error handling middleware
+// app.use(errorHandler);
+
+// Start the server
+// app.listen(port, () => {
+//     console.log(Server is running on port ${port});
+// });
+
+// env file configz
+const dotenv=require("dotenv");
+dotenv.config();
+connectDb();
+const app = express();
+app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname + '/views/partials'); // Path to your partials
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/register",require("./routes/userRoutes"));
+app.use("/api/doctor", require("./routes/doctorRoutes"))
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+    // req.file is the avatar file
+    // req.body will hold the text fields, if there were any
+    console.log(req.body);
+    console.log(req.file);
+return res.redirect("/home");
   })
 
-  const port = process.env.PORT || 5000;
-  // jha package.json hoti hai whi installation hoti hai
-
-  // Server listens on the defined port
-  app.get("/users",(req,res)=>{
-      res.render("users",{
-
-          people:[
-              {
-                  username:"himesh",
-                  age:20
-              },
-              {
-                  username:"saini",
-                  age:21
-              }
-          ]
-
-      })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
   })
+  
+  const uploads = multer({ storage: storage })
 
-  app.listen(port, () => {
-      console.log( `Server running at http://localhost:${port}/`);
-    });
+
+
+app.get("/home",(req,res)=>{
+
+    res.render("home",{
+        username:"Krishna",
+        
+    })
+})
+
+const port = process.env.PORT || 5000;
+// jha package.json hoti hai whi installation hoti hai
+
+// Server listens on the defined port
+app.get("/users",(req,res)=>{
+    res.render("users",{
+
+        people:[
+            {
+                username:"Krihsna",
+                age:20
+            },
+            {
+                username:"Lakshay",
+                age:21
+            }
+        ]
+
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
